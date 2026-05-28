d87c8caab6fe1d7bba85f5c833c569dda1fdea16
CHOIMINJUN(최민준) <s24071@gsm.hs.kr>
2026-05-28 16:45:44 +0900

    add Role skill to none
    add Role skill to none

--- Assets/Rpg/Role.cs
+++ Assets/Rpg/Role.cs
@@ -5,17 +5,22 @@ public enum RoleSkills
     Warrior,
     Rogue,
     Archer,
-    Wizard
+    Wizard,
+    None
 }
 
 public class Role : MonoBehaviour
 {
-    public RoleSkills roleSkill;
+    public RoleSkills roleSkill = RoleSkills.None;
     public GameObject Choose;
-
+    
     // 핵심: static 액션을 선언하여 어디서든 구독 가능하게 함
     public static Action<RoleSkills> OnRoleChosen;
-
+    public void Start()
+    {
+        roleSkill = (RoleSkills)PlayerPrefs.GetInt("SelectedRole", 0);
+        OnRoleChosen?.Invoke(roleSkill);
+    }
     public void chooseRole(int index)
     {
         roleSkill = (RoleSkills)index;
